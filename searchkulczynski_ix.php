7d1e59ba45cd47b22cf42a08bb15aec03c81f613
AmarYunan <56033863+AmarYunan@users.noreply.github.com>
2020-12-22 16:47:41 +0700

    Add files via upload

---
+++ searchkulczynski_ix.php
@@ -0,0 +1,13 @@
+<?php
+include "index.php";
+include "koneksi.php";
+?>
+<html>
+	<form action='searchskulczynski_i3.php' method='GET'>
+    	  <center> 
+        	<p><font size="5" face="sans-serif"><br>
+			    <img src="assets/images/20201101_160552.png" alt="Google Title">
+            <br><input type='text' size='50' name='keyword'> <input type='submit' value='Cari'></font></p>   
+      </center>
+      </form>
+</html>
